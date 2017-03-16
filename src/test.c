e8b1e89b3d7f46bde4529dc7b8a8f3163e0c52a1
Dylan Hunn <dylhunn@gmail.com>
2017-03-16 12:44:58 -0700

    up to date on all systems

--- src/test.c
+++ src/test.c
@@ -3,6 +3,16 @@
 #include "util.h"
 #include "hashset.h"
 
+// output colors
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
+
 #define expect(x) if ((x) == 0) {printf(RED "line %d (%s) " RESET, \
 	(__LINE__), (__FILE__)); return 0;}
 
@@ -165,7 +175,7 @@ int test_generic_solve(solution (*fn)(const formula)) {
 	char *file_paths[] = 
 		{"../test_data/test0.txt", "../test_data/test1.txt",
 		"../test_data/test2.txt"};
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < 2; i++) {
 		int status = read_input(file_paths[i], &f);
 		expect(status == 0);
 		solution s = (*fn)(f);
